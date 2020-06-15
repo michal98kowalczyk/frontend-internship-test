68169e33ef42a9e28a169cb7367ef800c1e7f7bc
Michal Kowalczyk <michal98kowalczyk@gmail.com>
2020-06-15 21:16:56 +0200

    task 1: popup with closing button

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
 /* Here goes your JS code */
+
+const message = document.querySelector(".main .message");
+const btnClickMe = document.querySelector(".main button");
+const btnExit = document.querySelector(".popup .exit");
+const popup = document.querySelector(".popup");
+const email = document.querySelector("form .email");
+const password = document.querySelector("form .password");
+const terms = document.querySelector("form .terms");
+const btnSubmit = document.querySelector("form .submit");
+
+let isEmailCorrect = false;
+let isPasswordCorrect = false;
+let isCheckboxSelected = false;
+
+
+
+function showPopup() {
+    popup.classList.toggle('active');
+    btnClickMe.classList.remove('active');
+}
+
+function hidePopup() {
+    popup.classList.remove('active');
+    btnClickMe.classList.add('active');
+}
+// showPopup();
+btnClickMe.addEventListener('click', showPopup);
+btnExit.addEventListener('click', hidePopup);
